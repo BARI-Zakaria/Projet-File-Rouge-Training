f8092166a5a4b6f7684ba0048f729cd744b42443
BARI-Zakaria <zakariabari213@gmail.com>
2022-06-18 13:46:20 +0100

    Check out COMPLETED

--- checkOut.php
+++ checkOut.php
@@ -1,10 +1,39 @@
 <?php
 include 'connect.php';
 
+// GET THE PRODUCT WITH IT IMAGE AND DETAILS
 $id = "";
 if(isset($_GET["id"])){
     $id = $_GET["id"];
 }
+
+$sqlQuery1 = "SELECT * FROM `produit` P
+            WHERE P.idProduit = $id";
+
+$sqlQuery2 = "SELECT * FROM `produit` P  
+            INNER JOIN `media` M 
+            ON P.idProduit = M.idProduit
+            WHERE P.idProduit = $id";
+$result1 = mysqli_query($connect, $sqlQuery1);
+$result2 = mysqli_query($connect, $sqlQuery2);
+
+
+// INSERT INTO RESERVATION TABLE
+if(isset($_POST['valid'])){
+$adr = $_POST['adresse'];
+$date = $_POST['date'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];    
+$sql = "INSERT INTO reservation (`adresseLiv`, `dateCommande`, `teleReserv`, `emailReserv`) VALUES ('$adr', '$date', '$phone', '$email')";
+$query = mysqli_query($connect, $sql);
+if($query){
+    // header('location:ch.php');
+    echo "COMMANDE IS INSERT";
+}
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,5 +57,80 @@ if(isset($_GET["id"])){
         </div>
 
     </nav>
+    <div class="container">
+
+        <form action="" method="POST">
+            <?php
+                
+                if(mysqli_num_rows($result2) > 0){
+                    
+                    ?>
+                    <div class="box1">
+                        <?php   
+                    while($row = mysqli_fetch_assoc($result2)){
+                        
+                        ?>
+                    <img src="<?php  echo 'Images/Matériels/proPics/'. $row["urlMedia"];?>" class="card-img-top" alt="ProductImage">
+                    <?php
+                                                
+                                            }
+                                            
+                                            $product = mysqli_fetch_assoc($result1);
+                                            ?>
+                    </div>
+            <div class="reserv">
+                    <h4 class="card-title" id="h4"><span class="bold"><?php echo  $product["nomProduit"]. ' .';?></span></h4>
+
+                    <div class="input-group mb-3">
+                        <label for="staticText" class="col-sm-2 col-form-label" id="label">Date :</label>
+                        <span class="text">Du</span>
+                        <input type="date" class="form-control" placeholder="Username" name="date">
+                        <!-- <span class="text">jusqu'à</span>
+                        <input type="date" class="form-control" placeholder="Server" name="date"> -->
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label for="staticText" class="col-sm-2 col-form-label">Adresse livraison</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control-plaintext" id="staticText" placeholder="Entrer une adresse de livraison" name="adresse">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="staticPhone" class="col-sm-2 col-form-label">Téléphone</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control-plaintext" id="staticPhone" placeholder="06******00" name="phone">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">E-mail</label>
+                        <div class="col-sm-10">
+                        <input type="text"  class="form-control-plaintext" id="staticEmail" placeholder="Entrer votre adresse E-mail" name="email">
+                        </div>
+                    </div>
+
+                <?php
+                }
+                ?>
+                <button type="submit" name="valid" id="check">
+                <a href="checkOut.php?id=<?php echo $product["idProduit"];?>" id="a">Réserver
+                    <i class="fa-solid fa-circle-check"></i>
+                </a>
+                </button>   
+            </div>
+        </form>
+    
+    </div>
 </body>
+<footer>
+        <div class="footer">
+            <img src="Images/kirae.png" alt="kiraeLogo" id="img2">
+            <div class="SMedia">
+                <p id="SMedia">Suivez-nous sur :</p>
+                <i class="fa-brands fa-facebook-square"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+            </div>
+        </div>
+        <p id="copyright">Copyrights © 2022 All Rights Reserved by Kirae.ma .</p>
+    </footer>
 </html>
